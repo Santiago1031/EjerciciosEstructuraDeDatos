7edfbb531e32febb1d6ed2b75f9b85b80d67c808
Santiago Cordoba <scordoba067@soyudemedellin.edu.co>
2023-03-07 20:09:38 -0500

    implement exercise 2

--- exercises/exercise_2.py
+++ exercises/exercise_2.py
@@ -1,16 +1,27 @@
-def main():
-    strn1 = "2468"
-    val = int(strn1)
-    print("String value = ", strn1)
-    print("Integer value = ", val)
 
-    strn2 = "Colombia"
-    try:
-        val = int(strn2)
-    except ValueError:
-        val = 0
-        print("String value = ", strn2)
-        print("Integer value = ", val)
+def myAtoiRecursive(string, num):
+    # Si str es NULL o str contiene no numérico
+    # caracteres luego devuelven 0 ya que el número no es
+    # válido
+    if string.isalpha():
+        return 0;
+
+    if (len(string) == 0):
+        return 0;
+    # caso base, hemos llegado al final de la cadena,
+    # simplemente devolvemos el último valor
+    if len(string) == 1:
+        return int(string) + (num * 10)
+
+    # agregar el siguiente elemento de cadena en nuestro valor numérico
+    num = int(string[0:1]) + (num * 10)
+
+    # recurse por el resto de la cadena
+    # y agregue cada letra a num
+    return myAtoiRecursive(string[1:], num)
 
 
-main()
+# código de conductor
+string = "103"
+
+print(myAtoiRecursive(string, 3))
